b2b8c6a8a20349807158a81fdcebe4b503d44c0c
Dmitry Godz <woke@mail.ru>
2023-06-19 23:12:01 +0300

    m2m in progress

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-
 from .models import Article
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     pass
+
+
+# @admin.register(Tag)
+# class ArticleAdmin(admin.ModelAdmin):
+#     pass
